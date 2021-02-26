9d8cb0df6dbc3eb99f368da877fea517502cdb2d
Goran <goranfrance@hotmail.com>
2021-02-26 21:01:36 +0000

    oush

---
+++ main.js
@@ -0,0 +1,14 @@
+const APIURL = 'https://api.github.com/users/';
+
+getUser("goransuljak")
+
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username)
+
+        console.log(data)
+    }
+    catch(err) {
+        console.log(err)
+    }
+}
