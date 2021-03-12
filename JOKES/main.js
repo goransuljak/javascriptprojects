f723e5b8f75eee27bd6a369d9e08217a82ffc9ff
Goran <goranfrance@hotmail.com>
2021-03-12 11:56:41 +0000

    fasd

---
+++ JOKES/main.js
@@ -0,0 +1,22 @@
+const jokeEl = document.getElementById("joke")
+const jokeBtn = document.getElementById("joke-btn")
+
+
+generateJoke()
+
+jokeBtn.addEventListener("click", generateJoke)
+
+
+function generateJoke() {
+    const config = {
+        headers: {
+            "Accept" : "application/json"
+        }
+    }
+    fetch('https://icanhazdadjoke.com/', config)
+    .then(res => res.json()) 
+    .then(data => {
+        jokeEl.innerHTML = data.joke
+    })
+}
+
