87a639e132bc96766ccc1591bd20517412bb0c49
Goran <goranfrance@hotmail.com>
2021-03-12 10:51:43 +0000

    d

--- CONTENT-PLACEHOLDER/main.js
+++ CONTENT-PLACEHOLDER/main.js
@@ -8,7 +8,7 @@ const date = document.getElementById('date')
 const animated_bgs = document.querySelectorAll('.animated-bg')
 const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
 
-setTimeout(getData, 2000)
+setTimeout(getData, 3000)
 function getData() {
     header.innerHTML = '<img src="https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" alt="" />'
 
