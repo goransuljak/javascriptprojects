4ce36e4e2ddeafbee85c8aa330476233299d7acd
Goran <goranfrance@hotmail.com>
2021-03-13 11:39:50 +0000

    dfa

--- INCEST-GAME/main.js
+++ INCEST-GAME/main.js
@@ -1,3 +1,37 @@
 const screens = document.querySelectorAll(".screen")
-
 const choose_insect_btns = document.querySelectorAll(".choose-insect-btn")
+const timeEl = document.getElementById("time")
+const scoreEl = document.getElementById("score")
+const messageEl = document.getElementById("message")
+const start_btn = document.getElementById("btn-start")
+const game_container = document.getElementById("game-container")
+
+let seconds = 0
+let score = 0
+let selected_insect = {}
+
+start_btn.addEventListener("click", () => screens[0].classList.add("up"))
+
+choose_insect_btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const img = btn.querySelector("img")
+        const src = img.getAttribute("src")
+        const alt = img.getAttribute("alt")
+
+        selected_insect = { src, alt }
+        screens[1].classList.add("up")
+
+        setTimeout(createInsect, 1000)
+        startGame()
+    })
+})
+
+function  createInsect() {
+    const insect = document.createElement("div")
+    insect.classList.add("insect")
+
+    const { x, y } = getRandomLocation()
+}
+function getRandomLocation() {
+    
+}
