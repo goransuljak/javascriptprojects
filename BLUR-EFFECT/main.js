e3ced304e797d5691f9d72a42ae558703da1975a
Goran Suljak <goranfrance@hotmail.com>
2021-04-23 10:17:59 +0100

    rfe

--- BLUR-EFFECT/main.js
+++ BLUR-EFFECT/main.js
@@ -8,15 +8,15 @@ let int = setInterval(blurring, 30);
 
 function blurring() {
     load++
-    if(load > 99) {
+    if (load > 99) {
         clearInterval(int)
     }
 
     loadText.innerText = `${load}%`
-    loadText.style.opacity = scale(load, 0, 100, 1,0)
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
     bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
-function scale (number, inMin, inMax, outMin, outMax) {
+function scale(number, inMin, inMax, outMin, outMax) {
     return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
